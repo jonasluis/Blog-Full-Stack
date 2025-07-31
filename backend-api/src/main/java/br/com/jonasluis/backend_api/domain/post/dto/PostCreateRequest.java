04aa326dbad7a4a58b8261952b8b96f9264e8a94
jonasluis <jonasluis66@gmail.com>
2025-07-31 19:41:02 -0300

    remove: remove import

--- backend-api/src/main/java/br/com/jonasluis/backend_api/domain/post/dto/PostCreateRequest.java
+++ backend-api/src/main/java/br/com/jonasluis/backend_api/domain/post/dto/PostCreateRequest.java
@@ -1,9 +1,5 @@
 package br.com.jonasluis.backend_api.domain.post.dto;
 
-import jakarta.persistence.ElementCollection;
-import jakarta.validation.constraints.NotBlank;
-
-import java.time.LocalDate;
 import java.util.List;
 
 public record PostCreateRequest(
